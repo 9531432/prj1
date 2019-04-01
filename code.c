5a1d516d42defbc65d2817cbfdc428e1b5d73d12
9531432 <M.mahdavim21@gmail.com>
2019-04-01 13:48:11 +0430

    question2

--- code.c
+++ code.c
@@ -1,40 +1,73 @@
+#include <sys/types.h> 
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h>
+
+
+void check_sort(int array[], int length) 
+{ 
+	if (length==1) 
+	{ 
+		printf("array is sorted\n"); 
+		return; 
+	} 
+
+	int i; 
+	for (i=1; i<length; i++) 
+	{ 
+		if (array[i]<array[i-1]) 
+		{ 
+			printf("array is not sorted\n"); 
+			return; 
+		} 
+	} 
+	printf("Sorting Done Successfully\n"); 
+	return; 
+} 
+
+
+
+
+
 int main() 
 { 
 	int shmid; 
 	key_t key = IPC_PRIVATE; 
-	int *shm_array; 
+	int *array; 
  
 	int length = 10000; 
 
-	size_t SHM_SIZE = sizeof(int)*length; 
+	size_t SIZE = sizeof(int)*length; 
 
-	if ((shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666)) < 0) 
+	if ((mid = shmget(key, SIZE, IPC_CREAT | 0666)) < 0) 
 	{ 
 		perror("shmget"); 
 		_exit(1); 
 	} 
  
-	if ((shm_array = shmat(shmid, NULL, 0)) == (int *) -1) 
+	if ((array = shmat(mid, NULL, 0)) == (int *) -1) 
 	{ 
 		perror("shmat"); 
 		_exit(1); 
 	} 
 
 	srand(time(NULL)); 
-	fillData(shm_array, length); 
+	fillData(array, length); 
 
-	mergeSort(shm_array, 0, length-1); 
+	mergeSort(array, 0, length-1); 
  
-	isSorted(shm_array, length); 
+	check_sort(array, length); 
 
 	
-	if (shmdt(shm_array) == -1) 
+	if (shmdt(array) == -1) 
 	{ 
 		perror("shmdt"); 
 		_exit(1); 
 	} 
 
-	if (shmctl(shmid, IPC_RMID, NULL) == -1) 
+	if (shmctl(mid, IPC_RMID, NULL) == -1) 
 	{ 
 		perror("shmctl"); 
 		_exit(1); 
